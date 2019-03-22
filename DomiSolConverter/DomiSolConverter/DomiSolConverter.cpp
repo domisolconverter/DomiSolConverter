36e0ea1ad3a918960d9259ac1cd7b1b841c0be02
tyg04308 <tyg04308@naver.com>
2019-03-23 00:45:55 +0900

    Init Project But not work

--- DomiSolConverter/DomiSolConverter/DomiSolConverter.cpp
+++ DomiSolConverter/DomiSolConverter/DomiSolConverter.cpp
@@ -1,7 +1,24 @@
-﻿#include "pch.h"
-#include <iostream>
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "pch.h"
+
+using namespace std;
+//using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	cout << "OpenCV Version : " << CV_VERSION << endl;
+	cv::Mat img;
+	cv::namedWindow("EXAMPLE01", CV_WINDOW_AUTOSIZE);
+
+	img = cv::imread("test.jpg", CV_LOAD_IMAGE_COLOR);
+	if (img.empty())
+	{
+		cout << "[!] You can NOT see the cat!" << endl;
+		return -1;
+	}
+	imshow("EXAMPLE01", img);
+	cv::waitKey(0);
+	cv::destroyWindow("EXAMPLE01");
+	return 0;
 }
