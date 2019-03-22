a2011053ee5d8340a56bf33957624e292ec8f108
simba328 <simba328@naver.com>
2019-03-23 01:08:22 +0900

    Init Project work

--- DomiSolConverter/DomiSolConverter/DomiSolConverter.cpp
+++ DomiSolConverter/DomiSolConverter/DomiSolConverter.cpp
@@ -1,24 +1,24 @@
 ﻿#include <iostream>
-#include <opencv2/opencv.hpp>
 #include "pch.h"
+#include <opencv2/opencv.hpp>
 
 using namespace std;
-//using namespace cv;
+using namespace cv;
 
 int main()
 {
 	cout << "OpenCV Version : " << CV_VERSION << endl;
-	cv::Mat img;
-	cv::namedWindow("EXAMPLE01", CV_WINDOW_AUTOSIZE);
+	Mat img;
+	namedWindow("EXAMPLE01", CV_WINDOW_AUTOSIZE);
 
-	img = cv::imread("test.jpg", CV_LOAD_IMAGE_COLOR);
+	img = imread("test.jpg", CV_LOAD_IMAGE_COLOR);
 	if (img.empty())
 	{
 		cout << "[!] You can NOT see the cat!" << endl;
 		return -1;
 	}
 	imshow("EXAMPLE01", img);
-	cv::waitKey(0);
-	cv::destroyWindow("EXAMPLE01");
+	waitKey(0);
+	destroyWindow("EXAMPLE01");
 	return 0;
 }
