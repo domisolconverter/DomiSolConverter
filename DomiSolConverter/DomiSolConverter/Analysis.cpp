8d5dffbb0e8bc89b0f6df7aba46c29be0b33088a
wkdthf21 <wkdthf21@naver.com>
2019-04-07 02:25:46 +0900

    modify rect

--- DomiSolConverter/DomiSolConverter/Analysis.cpp
+++ DomiSolConverter/DomiSolConverter/Analysis.cpp
@@ -12,11 +12,172 @@ void DomiSolConverter::Analysis::calculateStaffSpace(){
 
 void DomiSolConverter::Analysis::calculateStaffXY(){
 	
-	// 악보 한마디(20%)가 되면 오선이라 판단
-	
+	// (수정)DomiSolConverter의 straitenedImg로 바꾸세요
+	string INPUTPATH = "./inputImage/staffLine.jpg";
 
-	// 수직 히스토그램 그려면서
-	// 각 줄의 오선의 첫번째 위치와 마지막을 저장
+	Mat src = imread(INPUTPATH, IMREAD_GRAYSCALE);
+
+	//cout<<"Creat Analysis Objects!"<<endl;
+	//cout << "Access OuterClass Instance From InnerClass"<<endl;
+
+	int width = src.cols;
+	int height = src.rows;
+	int oneBar = (int)((width / 100.0) * 20);    // 140
+	int colCnt = 0;
+	int staffNum = 0;
+	vector<int> Y;
+	vector<int> X;
+
+
+	// Y축 히스토그램 그리기 //
+
+	// 악보 한마디(20%)가 되면 오선이라 판단
+
+	for (int nr = 0; nr < height; nr++) {
+
+
+		uchar* pixel = src.ptr<uchar>(nr);
+		list<int> tempX;
+		colCnt = 0;
+
+		for (int nc = 0; nc < width; nc++) {
+
+			if (pixel[nc]>230) {
+				tempX.push_back(nc);
+				colCnt++;
+			}
+		}
+		/*
+		if (colCnt != 0) {
+		cout <<  "의심 " << nr <<" 값 " <<  colCnt << endl;
+		}
+		*/
+
+		if (colCnt >= oneBar) {
+
+			Y.push_back(nr);
+			X.push_back(tempX.front());
+			X.push_back(tempX.back());
+		}
+
+		// Y축 히스토그램 완성
+		for (int nc = 0; nc < colCnt; nc++) {
+			pixel[nc] = pixel[nc] / 64 * 64 + 128 / 2;
+		}
+
+	}
+
+	// 악보 라인 보정
+
+	for (int i = 1; i < Y.size(); i++) {
+		if (Y[i] - Y[i - 1] <= 2) {
+			Y.erase(Y.begin() + i - 1);
+			X.erase(X.begin() + 2 * i);
+			X.erase(X.begin() + 2 * i + 1);
+		}
+	}
+
+	for (int i = 0; i < Y.size(); i++) {
+		cout << Y[i] << " ";
+	}
+
+	// 최소3개 악보 간격보다 2배 이상 커지면
+	// 다음 오선으로 판단
+	int lineCnt = 0;
+	int spaceCnt = 0;
+	int diff = 0;
+	int diffAvg = 0;
+	for (int i = 1; i < Y.size(); i++) {
+		if (spaceCnt == 0) {
+
+			diff = Y[i] - Y[i - 1];
+			spaceCnt++;
+
+		}
+		else {
+			// 평균 구하기
+			diffAvg = diff / spaceCnt;
+			// 평균의 200%보다 차이가 크지 않다면 추가
+			if (diffAvg * 2.5 >= Y[i] - Y[i - 1]) {
+				diff += Y[i] - Y[i - 1];
+				spaceCnt++;
+
+			}
+			// 크다 그리고 만약 그게 3줄 이상이면 그 전까지를 staff 하나라고 생각하기!
+			else {
+
+				cout << "현재 평균 " << diffAvg << endl;
+				cout << "간격 " << spaceCnt << endl;
+
+				if (spaceCnt >= 3) {
+					lineCnt++;
+					cout << lineCnt << "번 째 오선이네요 첫번째 줄 y는 " << Y[i - 1 - spaceCnt] << endl;
+					cout << lineCnt << "번 째 오선이네요 마지막 줄 y는 " << Y[i] << endl;
+
+					this->staffXY.push_back(Point(X[(i - 1 - spaceCnt) * 2], Y[i - 1 - spaceCnt]));
+					this->staffXY.push_back(Point(X[(i - 1) * 2 + 1], Y[i - 1]));
+
+				}
+				else {
+					cout << "얘는 오선이 아니에요" << endl;
+
+				}
+				spaceCnt = 0;
+				diff = 0;
+				diffAvg = 0;
+
+			}
+
+			// 배열이 끝나고 마지막 staff 체크
+			if (i == Y.size() - 1 && spaceCnt >= 3) {
+				lineCnt++;
+				cout << lineCnt << "번 째 오선이네요" << endl;
+				this->staffXY.push_back(Point(X[(i - spaceCnt) * 2], Y[i - spaceCnt]));
+				this->staffXY.push_back(Point(X[i * 2 + 1], Y[i]));
+			}
+
+		}
+
+
+	}
+
+
+	for (int i = 0; i < staffXY.size(); i++) {
+		cout << "( " << this->staffXY[i].x << " , " << this->staffXY[i].y << " )" << endl;
+	}
+
+	/* 애국가 테스트 프린트
+	Rect rect1(0, staffXY[0].y, width, staffXY[1].y - staffXY[0].y);
+	Mat subimage1 = src(rect1);
+	namedWindow("RECT01", CV_WINDOW_AUTOSIZE);
+	imshow("RECT01", subimage1);
+
+	Rect rect2(0, staffXY[2].y, width, staffXY[3].y - staffXY[2].y);
+	Mat subimage2 = src(rect2);
+	namedWindow("RECT02", CV_WINDOW_AUTOSIZE);
+	imshow("RECT02", subimage2);
+
+	Rect rect3(0, staffXY[4].y, width, staffXY[5].y - staffXY[4].y);
+	Mat subimage3 = src(rect3);
+	namedWindow("RECT03", CV_WINDOW_AUTOSIZE);
+	imshow("RECT03", subimage3);
+
+	Rect rect4(0, staffXY[6].y, width, staffXY[7].y - staffXY[6].y);
+	Mat subimage4 = src(rect4);
+	namedWindow("RECT04", CV_WINDOW_AUTOSIZE);
+	imshow("RECT04", subimage4);
+	*/
+
+	Rect rect[10]; // (수정)
+
+	for (int i = 0; i < staffXY.size()/2; i++) {
+		rect[i] = Rect(0, staffXY[i].y, width, staffXY[i+1].y - staffXY[i].y); // (수정)평균 오선 크기 넣기
+	}
+
+	namedWindow("OUT01", CV_WINDOW_AUTOSIZE);
+	imshow("OUT01", src);
+
+	destroyWindow("OUT01");
 
 }
 
@@ -46,168 +207,8 @@ void DomiSolConverter::Analysis::recognizeNoteSymbol() {
 
 
 DomiSolConverter::Analysis::Analysis() {
-
-	// (수정)DomiSolConverter의 straitenedImg로 바꾸세요
-	string INPUTPATH = "./inputImage/staffLine.jpg";
 	
-	Mat src = imread(INPUTPATH, IMREAD_GRAYSCALE);
-
-	//cout<<"Creat Analysis Objects!"<<endl;
-	//cout << "Access OuterClass Instance From InnerClass"<<endl;
-
-	int width = src.cols;
-	int height = src.rows;
-	int oneBar = (int)((width / 100.0) * 20);    // 140
-	int colCnt = 0;
-	int staffNum = 0;
-	vector<int> Y;
-	vector<int> X;
-
-
-	// Y축 히스토그램 그리기 //
-
-	// 악보 한마디(20%)가 되면 오선이라 판단
-
-	for (int nr = 0; nr < height; nr++) {
-		
-		
-		uchar* pixel = src.ptr<uchar>(nr);
-		list<int> tempX;
-		colCnt = 0;
-
-		for (int nc = 0; nc < width; nc++) {
-			
-			if (pixel[nc]>230) {
-				tempX.push_back(nc);
-				colCnt++;
-			}
-		}
-		/*
-		if (colCnt != 0) {
-			cout <<  "의심 " << nr <<" 값 " <<  colCnt << endl;
-		}
-		*/
-
-		if (colCnt >= oneBar) {
-			
-			Y.push_back(nr);
-			X.push_back(tempX.front());
-			X.push_back(tempX.back());
-		}
-
-		// Y축 히스토그램 완성
-		for (int nc = 0; nc < colCnt; nc++) {
-			pixel[nc] = pixel[nc] / 64 * 64 + 128 / 2;
-		}
-
-	}
-
-	// 악보 라인 보정
-
-	for (int i = 1; i < Y.size(); i++) {
-		if (Y[i] - Y[i-1] <= 2) {
-			Y.erase(Y.begin() + i - 1);
-			X.erase(X.begin() + 2 * i);
-			X.erase(X.begin() + 2 * i + 1);
-		}
-	}
-
-	for (int i = 0; i < Y.size(); i++) {
-		cout << Y[i] << " ";
-	}
-
-	// 최소3개 악보 간격보다 2배 이상 커지면
-	// 다음 오선으로 판단
-	int lineCnt = 0;
-	int spaceCnt = 0;
-	int diff = 0;
-	int diffAvg = 0;
-	for (int i = 1; i < Y.size(); i++) {
-		if (spaceCnt == 0) {
-
-			diff = Y[i] - Y[i-1];
-			spaceCnt++;
-
-		}
-		else {
-			// 평균 구하기
-			diffAvg = diff / spaceCnt;
-			// 평균의 200%보다 차이가 크지 않다면 추가
-			if (diffAvg * 2.5 >= Y[i] - Y[i-1]) {
-				diff += Y[i] - Y[i-1];
-				spaceCnt++;
-
-			}
-			// 크다 그리고 만약 그게 3줄 이상이면 그 전까지를 staff 하나라고 생각하기!
-			else {
-
-				cout << "현재 평균 " << diffAvg << endl;
-				cout << "간격 "<< spaceCnt << endl;
-
-				if (spaceCnt >= 3) {
-					lineCnt++;
-					cout << lineCnt << "번 째 오선이네요 첫번째 줄 y는 " << Y[i-1-spaceCnt] << endl;
-					cout << lineCnt << "번 째 오선이네요 마지막 줄 y는 " << Y[i] << endl;
-
-					this->staffXY.push_back(Point(X[(i - 1 - spaceCnt) * 2], Y[i - 1 - spaceCnt]));
-					this->staffXY.push_back(Point(X[(i - 1) * 2 + 1], Y[i - 1]));
-
-				}
-				else {
-					cout << "얘는 오선이 아니에요" << endl;
-					
-				}
-				spaceCnt = 0;
-				diff = 0;
-				diffAvg = 0;
-
-			}
-
-			// 배열이 끝나고 마지막 staff 체크
-			if (i == Y.size() - 1 && spaceCnt >= 3) {
-				lineCnt++;
-				cout << lineCnt << "번 째 오선이네요" << endl;
-				this->staffXY.push_back(Point(X[(i - spaceCnt) * 2], Y[i - spaceCnt]));
-				this->staffXY.push_back(Point(X[i * 2 + 1], Y[i]));
-			}
-			
-		}
-		
-
-	}
-
-	
-	for (int i = 0; i < staffXY.size(); i++) {
-		cout << "( " << this->staffXY[i].x << " , " << this->staffXY[i].y <<" )" << endl;
-	}
-	
-	
-	Rect rect1(0, staffXY[0].y, width, staffXY[1].y - staffXY[0].y);
-	Mat subimage1 = src(rect1);
-	namedWindow("RECT01", CV_WINDOW_AUTOSIZE);
-	imshow("RECT01", subimage1);
-	
-	Rect rect2(0, staffXY[2].y, width, staffXY[3].y - staffXY[2].y);
-	Mat subimage2 = src(rect2);
-	namedWindow("RECT02", CV_WINDOW_AUTOSIZE);
-	imshow("RECT02", subimage2);
-
-	Rect rect3(0, staffXY[4].y, width, staffXY[5].y - staffXY[4].y);
-	Mat subimage3 = src(rect3);
-	namedWindow("RECT03", CV_WINDOW_AUTOSIZE);
-	imshow("RECT03", subimage3);
-
-	Rect rect4(0, staffXY[6].y, width, staffXY[7].y - staffXY[6].y);
-	Mat subimage4 = src(rect4);
-	namedWindow("RECT04", CV_WINDOW_AUTOSIZE);
-	imshow("RECT04", subimage4);
-	
-	
-
-	namedWindow("OUT01", CV_WINDOW_AUTOSIZE);
-	imshow("OUT01", src);
-
-	destroyWindow("OUT01");
+	calculateStaffXY();
 
 }
 
