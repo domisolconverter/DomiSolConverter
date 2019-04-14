6df1675021ae3bc167d3be4a61f432f39473eb1a
tyg04308 <tyg04308@naver.com>
2019-04-15 00:35:57 +0900

    add model file

--- DomiSolConverter/DomiSolConverter/main.cpp
+++ DomiSolConverter/DomiSolConverter/main.cpp
@@ -2,13 +2,18 @@
 
 #include "pch.h"
 #include "DomiSolConverter.h"
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 # define INPUTFOLDER "./inputImage/"
 # define OUTPUTFOLDER "./outputImage/"
 
 // Change test case
-string INPUTFILE = "noStaff.jpg";
+string INPUTFILE = "case2.jpg";
 string OUTPUTFILE = "";
+
+string xml = "output.xml";
+CascadeClassifier classifier;
 
 
 int main()
@@ -16,18 +21,25 @@ int main()
 	// Set path of input image and output image
 	string INPUTPATH = INPUTFOLDER + INPUTFILE;
 	string OUTPUTPATH = OUTPUTFOLDER + OUTPUTFILE;
+	
+	vector<Rect> sharps;
 
 	cout << "OpenCV Version : " << CV_VERSION << endl;
 	Mat img;
 	namedWindow("CASE01", CV_WINDOW_AUTOSIZE);
 
-	img = imread(INPUTPATH, CV_LOAD_IMAGE_COLOR);
+
+	img = imread(INPUTPATH, CV_LOAD_IMAGE_GRAYSCALE);
 	if (img.empty())
 	{
 		cout << "[!] You can NOT open the test image!" << endl;
 		return -1;
 	}
+
+	
+	//imwrite("test.jpg", img);
 	imshow("CASE01", img);
+
 	waitKey(0);
 	destroyWindow("CASE01");
 	return 0;
