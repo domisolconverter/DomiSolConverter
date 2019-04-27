dd098ef4a1633190cb03fa3faa107bc7aef1049d
tyg04308 <tyg04308@naver.com>
2019-04-28 02:11:18 +0900

    fetch from preprocessing branch

--- DomiSolConverter/DomiSolConverter/main.cpp
+++ DomiSolConverter/DomiSolConverter/main.cpp
@@ -2,18 +2,13 @@
 
 #include "pch.h"
 #include "DomiSolConverter.h"
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
 
 # define INPUTFOLDER "./inputImage/"
 # define OUTPUTFOLDER "./outputImage/"
 
 // Change test case
-string INPUTFILE = "case2.jpg";
+string INPUTFILE = "case1.jpg";
 string OUTPUTFILE = "";
-
-string xml = "output.xml";
-CascadeClassifier classifier;
 
 
 int main()
@@ -21,25 +16,23 @@ int main()
 	// Set path of input image and output image
 	string INPUTPATH = INPUTFOLDER + INPUTFILE;
 	string OUTPUTPATH = OUTPUTFOLDER + OUTPUTFILE;
-	
-	vector<Rect> sharps;
+
 
 	cout << "OpenCV Version : " << CV_VERSION << endl;
 	Mat img;
 	namedWindow("CASE01", CV_WINDOW_AUTOSIZE);
 
-
-	img = imread(INPUTPATH, CV_LOAD_IMAGE_GRAYSCALE);
+	img = imread(INPUTPATH, IMREAD_GRAYSCALE);
 	if (img.empty())
 	{
 		cout << "[!] You can NOT open the test image!" << endl;
 		return -1;
 	}
-
-	
-	//imwrite("test.jpg", img);
 	imshow("CASE01", img);
-
+	
+	
+	DomiSolConverter domisolconverter(img);
+	
 	waitKey(0);
 	destroyWindow("CASE01");
 	return 0;
